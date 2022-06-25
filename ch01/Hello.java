417616713a8211eab0ffa61297e92c8be25b8458
Tucker26 <108190126+Tucker26@users.noreply.github.com>
2022-06-25 18:38:50 -0500

    Removed the word static
    error testing

--- ch01/Hello.java
+++ ch01/Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
 
-    public static void mian(String[] args) {
+    public void main(String[] args) {
         // generate some simple output
         System.out.println("Hello, World!");
         System.out.println("How are you?");
