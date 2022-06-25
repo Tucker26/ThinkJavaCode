6264146ec5c89dcac1576e9cdb3073470448a157
Tucker26 <108190126+Tucker26@users.noreply.github.com>
2022-06-25 18:42:25 -0500

    Reset to original
    undo error testing

--- ch01/Hello.java
+++ ch01/Hello.java
@@ -2,7 +2,7 @@ public class Hello {
 
     public static void main(String[] args) {
         // generate some simple output
-        System.out.Println("Hello, World!");
+        System.out.println("Hello, World!");
         System.out.println("How are you?");
         //comment
     }
