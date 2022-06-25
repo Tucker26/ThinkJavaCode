3231afb09efafdff6eec6e3dd10bfc9e5c77d890
Tucker26 <108190126+Tucker26@users.noreply.github.com>
2022-06-25 18:39:22 -0500

    Removed the word public
    error testing

--- ch01/Hello.java
+++ ch01/Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
 
-    public void main(String[] args) {
+     static void main(String[] args) {
         // generate some simple output
         System.out.println("Hello, World!");
         System.out.println("How are you?");
