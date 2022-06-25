762438071251fbc424ccb1974e697fed54e7525f
Tucker26 <108190126+Tucker26@users.noreply.github.com>
2022-06-25 18:40:38 -0500

    Replaced println with Println
    error testing

--- ch01/Hello.java
+++ ch01/Hello.java
@@ -2,7 +2,7 @@ public class Hello {
 
     public static void main(String[] args) {
         // generate some simple output
-        .out.println("Hello, World!");
+        System.out.Println("Hello, World!");
         System.out.println("How are you?");
         //comment
     }
