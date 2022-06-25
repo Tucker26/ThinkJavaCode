f0ceae87e33e772a93116875529d738a26b30c37
Tucker26 <108190126+Tucker26@users.noreply.github.com>
2022-06-25 18:28:13 -0500

    Removed one of the close squiggly braces
    error testing

--- ch01/Hello.java
+++ ch01/Hello.java
@@ -1,10 +1,10 @@
 public class Hello {
 
-    public static void main(String[] args) 
+    public static void main(String[] args) {
         // generate some simple output
         System.out.println("Hello, World!");
         System.out.println("How are you?");
         //comment
-    }
+    
 
 }
